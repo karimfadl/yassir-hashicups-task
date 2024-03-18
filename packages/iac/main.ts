605ea1d966c1e86ee4e80e04e74ad156865d5559
Karim Fadl <karim.fadl>
2024-03-18 16:11:08 +0200

    Add Hashicups provider

--- packages/iac/main.ts
+++ packages/iac/main.ts
@@ -1,14 +1,59 @@
-import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { Construct } from "constructs";
+import * as fs from 'fs';
+import * as path from 'path';
+import { Order } from "./.gen/providers/hashicups/order/index.js";
+import { HashicupsProvider } from "./.gen/providers/hashicups/provider/index.js";
+
+
+interface MyStackConfig {
+  resourcePath: string[];
+}
 
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, config: MyStackConfig) {
     super(scope, id);
 
-    // define resources here
+    new HashicupsProvider(this, "hashicups", {
+      username: "education",
+      password: "test123",
+      host: "http://localhost:19090"
+    });
+
+    const resourcePath = config.resourcePath;
+    for (let index = 0; index < resourcePath.length; index++) {
+      new Order(this, config.resourcePath[index], {
+        items: loadItemList(`./resources/${resourcePath[index]}`),
+      }); 
+    }
   }
 }
 
+export function* readAllFiles(dir: string): Generator<string> {
+  const files = fs.readdirSync(dir, { withFileTypes: true });
+
+  for (const file of files) {
+    if (file.isDirectory()) {
+      yield* readAllFiles(path.join(dir, file.name));
+    } else {
+      if (!file.name.match('.DS_Store')) {
+        yield path.join(dir, file.name);
+      }
+    }
+  }
+  return files;
+}
+
+function loadItemList(dir: string) {
+  const itemsList = [];
+  for (const filePath of readAllFiles(dir)) {
+    const loadedItem = fs.readFileSync(filePath, 'utf-8');
+    const item = JSON.parse(loadedItem);
+    itemsList.push(item);
+  }
+  return itemsList;
+}
+
 const app = new App();
-new MyStack(app, "iac");
+new MyStack(app, "stack", { resourcePath: ["order1","order2"]});
 app.synth();
